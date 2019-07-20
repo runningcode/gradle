2aae8043d959b38281c428d2fd1f2cba84699727
Marc Philipp <marc@gradle.com>
2019-07-20 18:56:12 +0200

    Add spock-junit4

--- subprojects/internal-integ-testing/internal-integ-testing.gradle.kts
+++ subprojects/internal-integ-testing/internal-integ-testing.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
 
     implementation(library("groovy"))
     implementation(library("junit"))
-    implementation(testLibrary("spock"))
+    testLibraries("spock").forEach { implementation(it) }
     implementation(library("nativePlatform"))
     implementation(library("commons_lang"))
     implementation(library("commons_io"))
